d7ae77703998f0f769a7ea16df18739d981038cd
AsSidiiqqi12 <12350111224@students.uin-suska.ac.id>
2024-07-29 15:15:19 +0700

    tantangan devcode yang lumayan sukar

--- src/Tutorial/Latihan9.java
+++ src/Tutorial/Latihan9.java
@@ -2,42 +2,34 @@ package Tutorial;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Scanner;
 import java.util.TimeZone;
 
 public class Latihan9 {
     public static void main(String[] args) {
-        String format = "yyyy-MM-dd HH:mm:ss a";//format untuk tahun-bulan-hari jam:menit:detik AM/PM
-        SimpleDateFormat jakartaFormat = new SimpleDateFormat(format);//add format tadi
-        String waktuTempat = TimeZone.getDefault().getID();//default ID setempat (ASIA/JAKARTA)
-        System.out.println(waktuTempat);
-        Calendar cl = new GregorianCalendar();
-        TimeZone Jakrta = TimeZone.getTimeZone("Asia/Jakarta");
-        cl.setTimeZone(Jakrta);
-        System.out.println("Tanggal: "+ jakartaFormat.format(cl.getTime()));
-        System.out.println("Zona waktu: " + cl.getTimeZone().getID());
-        System.out.println("Nama zona waktu: " + cl.getTimeZone().getDisplayName());
+        Scanner sc = new Scanner(System.in);
+        int jumlahNegara = sc.nextInt();//untuk input jumlah negara yang ingin di konversi waktunya
+        sc.nextLine();//untuk membuat baris baru karena ada input int dan String secara berturut
+        String[] negaraZonaWaktu = new String[jumlahNegara];//array untuk menyimpan negara yang diinput user
+        for (int i = 0; i < jumlahNegara; i++) {//perulangan untuk input negara sebanyak yang diinginkan user
+            negaraZonaWaktu[i] = sc.nextLine();
+        }
+        System.out.println("==============================================");
+        for (String negara : negaraZonaWaktu){//perulangan untuk output negara serta tanggal dan zonaID nya perulangan ini menggunakan for each
+            konversiWaktu(negara);
+            System.out.println("==============================================");
+        }
 
-        System.out.println("========================================");
-
-        TimeZone Israel = TimeZone.getTimeZone("Israel");
-        SimpleDateFormat IsraelFormat = new SimpleDateFormat(format);
-        IsraelFormat.setTimeZone(Israel);
-        cl.setTimeZone(Israel);
-        System.out.println("Tanggal: "+ IsraelFormat.format(cl.getTime()));
-        System.out.println("Zona waktu: " + cl.getTimeZone().getID());
-        System.out.println("Nama zona waktu: " + cl.getTimeZone().getDisplayName());
-
-        System.out.println("========================================");
-
-
-        TimeZone GMT = TimeZone.getTimeZone("GMT");
-        SimpleDateFormat gmtFormat = new SimpleDateFormat(format);
-        gmtFormat.setTimeZone(GMT);
-        cl.setTimeZone(GMT);
-        System.out.println("Tanggal: " + gmtFormat.format(cl.getTime()));
-        System.out.println("Zona waktu: " + cl.getTimeZone().getID());
-        System.out.println("Nama Zona waktu: " + cl.getTimeZone().getDisplayName());
-
-
+    }
+    static void konversiWaktu (String zonaID){//function/method untuk konversi waktu negara
+        Calendar calendar = new GregorianCalendar();//objek kalender/deklarasi dengan GreogrianCalender => calander yang dipakai saat ini
+        String format = "dd-MM-yyyy HH:mm a";//setel format untuk waktu dan tanggal
+        SimpleDateFormat formatWaktu = new SimpleDateFormat(format);//format tanggal
+        TimeZone zonaIDtime = TimeZone.getTimeZone(zonaID);//pilih zona untuk konversi
+        formatWaktu.setTimeZone(zonaIDtime);//set format waktu berdasarkan zona ID
+        calendar.setTimeZone(zonaIDtime);// set calender nanti dipakai untuk zona waktu dan namanya
+        System.out.println("Tanggal        : " + formatWaktu.format(calendar.getTime()));
+        System.out.println("Zona waktu     : " + calendar.getTimeZone().getID());//mengeluarkan zona
+        System.out.println("Nama Zona waktu: " + calendar.getTimeZone().getDisplayName());//mengeluarkan waktu zona contohnya waktu indonesia barat
     }
 }
